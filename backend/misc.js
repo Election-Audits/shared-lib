976b3d454915ab1c5a1462427dc6b09ccd56538a
Etornam <rokrieg5@yahoo.com>
2024-09-16 18:58:39 +0000

    Added getJoiError, some English translations
    backend/misc: added getJoiError function
    locales/en: added some english translations

--- backend/misc.js
+++ backend/misc.js
@@ -2,12 +2,32 @@
 // miscellaneous backend functions
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.endpointError = endpointError;
+exports.getJoiError = getJoiError;
 const debug = require('debug')('ea:lib:misc');
 debug.log = console.log.bind(console);
-// handle endpoint error.
+/**
+ * Handle an error in an Express endpoint
+ * @param err Error
+ * @param req Express request
+ * @param res Express response
+ * @returns
+ */
 function endpointError(err, req, res) {
     debug('error in endpoint: ', req.url, '\n', err);
     if (err && err.errMsg)
         return res.status(400).send(err);
     return res.status(500).send('Something went wrong. Please try later');
+}
+/**
+ * Transform a Joi validation error to a human readable message
+ * @param error
+ * @returns
+ */
+function getJoiError(error) {
+    let msg = error.details[0].message;
+    //debug('msg: ', msg);
+    //if (msg.includes('empty')) return null; // ignore errors created by empty input
+    //let field = msg.split(' ')[0];//.replace('"', '');
+    //return {msg: `Invalid input: ${field}`};
+    return msg;
 }
--- backend/misc.ts
+++ backend/misc.ts
@@ -7,9 +7,30 @@ import * as express from "express";
 
 
 
-// handle endpoint error.
+/**
+ * Handle an error in an Express endpoint
+ * @param err Error
+ * @param req Express request
+ * @param res Express response
+ * @returns 
+ */
 export function endpointError(err: RequestError, req: express.Request, res: express.Response) {
     debug('error in endpoint: ', req.url, '\n', err);
     if (err && err.errMsg) return res.status(400).send(err);
     return res.status(500).send('Something went wrong. Please try later');
+}
+
+
+/**
+ * Transform a Joi validation error to a human readable message
+ * @param error 
+ * @returns 
+ */
+export function getJoiError(error: any) {
+    let msg = error.details[0].message;
+    //debug('msg: ', msg);
+    //if (msg.includes('empty')) return null; // ignore errors created by empty input
+    //let field = msg.split(' ')[0];//.replace('"', '');
+    //return {msg: `Invalid input: ${field}`};
+    return msg;
 }
